cc50e3463de16357c30c024fd73471879e18fae5
Bibekananda Datta <datta.bibek11@gmail.com>
2019-10-01 00:03:15 -0700

    comments on test functions

--- bintestfun.m
+++ bintestfun.m
@@ -1,11 +1,32 @@
 function [NMistake] = bintestfun(xdata,ydata,W)
 
-Y           = sign(W*xdata');
-NMistake    = 0;
+% bintestfun function is to predict binary classifiers
 
-for i = 1:length(Y)
+% Author    : Bibekananda Datta
+% Date      : 09/28/2019 
+
+% input arguments/ parameters:
+% xdata     : x test data as matrix (feature vectors as column)
+% ydata     : y test data as column vector
+% W         : learned weight vector
+
+% output arguments/ returning variables:
+% NMist     : no of mistakes in prediction
+
+Y           = sign(W*xdata');       %checks the sign of test data
+NMistake    = 0;                    %initialize mistake counter
+
+for i = 1:length(Y)             
     if Y(i) ~= ydata(i)
-        NMistake = NMistake +1;
+        NMistake = NMistake +1;     %updates counter for each mistake in prediction
     end
 end
 
+% this function can be modified to return accuracy instead of no of mistakes
+% following segment calculates accuracy
+% change output argument to Accuracy from NMistake for this purpose
+
+% Accuracy    = 1-NMistake/datasize;
+
+
+end
--- multitestfun.m
+++ multitestfun.m
@@ -1,28 +1,48 @@
 function [NMistake] = multitestfun(xts,yts,W,Nclass)
 
-trsize  = size(xts,1);
-ftsize  = size(xts,2);
-Y       = zeros(trsize,1);
+% bintestfun function is to predict binary classifiers
 
-F       = zeros(Nclass,ftsize);
+% Author    : Bibekananda Datta
+% Date      : 09/28/2019 
+
+% input arguments/ parameters:
+% xdata     : x test data as matrix (feature vectors as column)
+% ydata     : y test data as column vector
+% W         : learned weight vector
+% Nclass    : no of class in test dataset (should be same as training data)
+
+% output arguments/ returning variables:
+% NMist     : no of mistakes in prediction
+
+
+datasize    = size(xts,1);              %size of test data
+ftsize      = size(xts,2);              %size of feature vector
+Y           = zeros(datasize,1);        %initialzie of predicted Y vector
+F           = zeros(Nclass,ftsize);     %initialize augmented feature function
         
-for i = 1:trsize
-    for k = 1:Nclass
-        F(k,:)     = xts(i,:);
+for i = 1:datasize                      %loop through all data
+    for j = 1:Nclass
+        F(j,:)     = xts(i,:);          %create augmented feature function
     end
 
-    [maxm, index]   = max(dot(W',F'));
-    Y(i)            = index-1;
+    [~, index]   = max(dot(W',F'));     %argmax (w.F)
+    Y(i)            = index-1;          %adjusts index to class label prediction
 end
         
 
 
-NMistake    = 0;
+NMistake    = 0;                        %initialize mistake counter
 
 for i = 1:length(Y)
     if Y(i) ~= yts(i)
-        NMistake = NMistake +1;
+        NMistake = NMistake +1;         %updates counter for each mistake in prediction
     end
 end
+
+% this function can be modified to return accuracy instead of no of mistakes
+% following segment calculates accuracy
+% change output argument to Accuracy from NMistake for this purpose
+
+% Accuracy    = 1-NMistake/datasize;
 
 end
